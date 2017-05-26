ca523fd89fdb2a3e8d15f5be6a6f9698cec5a019
Rommel Dominguez <ynwodsun000@outlook.com>
2017-05-26 04:00:16 +0000

    modifications made to index.html

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mungo.MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017
   }
 
   a.set('view options',{layout: false});
-  a.use(e.static(__dirname + '/public'));
+  a.use(e.static(__dirname + '/'));
   a.get('/', function(pakiusap,tugon){
       tugon.render('index.html');
   });
